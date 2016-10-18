2bcf96c40da3c9f97475568e25ae4d82d5168e8e
Axel Martínez <amartinez@itexico.net>
2016-10-18 18:12:04 -0500

    Adding a welcome page

---
+++ client/welcome.js
@@ -0,0 +1,5 @@
+Template.welcome.helpers({
+	'get-started'() {
+		Router.go('home');
+	}
+});
--- routes.js
+++ routes.js
@@ -1,8 +1,18 @@
 Router.configure({
-  layoutTemplate: 'layout'
+  layoutTemplate: 'layout',
+  template: 'welcome'
 });
 
 Router.route('/', function () {
+  this.render('welcome');
+});
+
+Router.route('/machine', function () {
   this.render('home');
 });
 
+Router.route('/(.*)', {
+  action: function() {
+    Router.go('/');
+  }
+});
